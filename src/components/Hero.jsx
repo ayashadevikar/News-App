7cb813410b0746f2d400b6ed9123dd5bca1aa0d7
ayashadevikar <ayashadevikar8@gmail.com>
2023-09-24 23:21:34 +0530

    responsive navbar added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-slate-600 text-white  mt-2 p-40 font-bold text-3xl text-center flex flex-col gap-y-4'>
+    <div className='-ml-9 bg-slate-600 text-white  mt-2 p-40 font-bold text-3xl md:text-center flex flex-col gap-y-4'>
       <h1 className='text-orange-400'>NEWS Hub</h1>
       <h2>THE WEBSITE BUILD WITH THE NEWS API</h2>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
+ 
+  const [isOpen, setisOpen] = useState(false);
+
+  const toggleActiveClass = () => {
+    setisOpen(!isOpen);
+  }
+
   return (
     <> 
      {/* mobile view */}
   
-     <div>
-           
+     <div className='md:hidden p-2'>
+     <i onClick={toggleActiveClass} className=
+   {`fas ${isOpen ? "fa-times" : "fa-bars"}`}>
+   
+   </i>
+        
      </div>
+
+  
+     <ul className={`block bg-pink-200 w-full p-2 
+      ${isOpen ?  'block' : 'hidden' }`}>
+            <Link to="/"><li>Home</li></Link>
+            <Link to="/business"><li>Business</li></Link>
+            <Link to="/entertainment"><li>Entertainment</li></Link>
+            <Link to="/health"><li>Health</li></Link>
+            <Link to="/science"><li>Science</li></Link>
+            <Link to="/sports"><li>Sports</li></Link>
+            <Link to="/technology"><li>Technology</li></Link>
+        </ul>
 
 
     {/* laptop view */}
-      <div className='flex justify-between items-center mx-8 p-4'>
+      <div className='hidden md:flex justify-between items-center mx-8 p-4'>
 
       <h2 className='text-3xl font-bold'>NEWS App</h2>
        <div className='flex gap-x-4'>
