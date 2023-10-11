b202f4c02a12a70d3b3b130c73cba354cfa8337d
ayashadevikar <ayashadevikar8@gmail.com>
2023-10-11 21:44:27 +0530

    api updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = ({ cat }) => {
            const { data } = await axios.get(
            cat 
            ? `https://newsapi.org/v2/top-headlines?country=in&category=${cat}&apiKey=7041b7213f50405a972157577c7ad627`
-          :"https://newsapi.org/v2/top-headlines?country=in&apiKey=7041b7213f50405a972157577c7ad627"
+          :"http://newsapi.org/v2/top-headlines?country=in&apiKey=7041b7213f50405a972157577c7ad627"
           )
         
         console.log(data);
